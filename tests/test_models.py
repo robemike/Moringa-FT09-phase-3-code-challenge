ab35e55e28211a61a1b7fa2dbddc5ca01cbf6a5f
robemike <robemike1135@gmail.com>
2024-06-11 20:56:23 +0300

    .

--- tests/test_models.py
+++ tests/test_models.py
@@ -16,6 +16,53 @@ class TestModels(unittest.TestCase):
         magazine = Magazine(1, "Tech Weekly", "Technology")
         self.assertEqual(magazine.name, "Tech Weekly")
 
+    # Magazine class tests.
+    def test_id_getter(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        self.assertEqual(magazine.id, 1)
+
+    def test_name_getter(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        self.assertEqual(magazine.name, "Tech Weekly")
+
+    def test_category_getter(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        self.assertEqual(magazine.category, "Technology")
+
+    def test_id_setter(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        magazine.id = 2
+        self.assertEqual(magazine.id, 2)
+
+    def test_name_setter(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        magazine.name = "Science Monthly"
+        self.assertEqual(magazine.name, "Science Monthly")
+
+    def test_category_setter(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        magazine.category = "Science"
+        self.assertEqual(magazine.category, "Science")
+
+    # Author's class tests.
+    def author_test_id_getter(self):
+        author = Author(1, "John Doe")
+        self.assertEqual(author.id, 1)
+        
+    def test_id(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        self.assertEqual(magazine.id, 1)
+
+
+    def test_author_id(self):
+        author = Author(1, "John Doe")
+        self.assertEqual(author.id, 1)
+
+    def test_author_name(self):
+        author = Author(1, "John Doe")
+        self.assertEqual(author.name, "John Doe")
+
+    # Article class.
     def test_article_creation(self):
         article = Article(1, "Test Title", "Test Content", 1, 1)
         self.assertEqual(article.id, 1)
@@ -43,6 +90,21 @@ class TestModels(unittest.TestCase):
         self.assertEqual(articles[1].id, 2)
         self.assertEqual(articles[1].author_id, 2)
         self.assertEqual(articles[1].magazine_id, 2)
+
+    
+    def test_article_id(self):
+        article = Article(1, "article", "article-category", 1, 1)
+        self.assertEqual(article.id, 1)
+    
+    def test_article_title(self):
+        article = Article(1, "Article", "Article-category", 1, 1)
+        self.assertEqual(article.title, "Article")
+
+
+    def test_name(self):
+        magazine = Magazine(1, "Tech Weekly", "Technology")
+        self.assertEqual(magazine.name, "Tech Weekly")
+    
 
 if __name__ == "__main__":
     unittest.main()
